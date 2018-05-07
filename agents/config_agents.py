e588210b2e3b113042e50b04dd8eecfb8ac41b11
Sangwoo Moon <swmoon00@gmail.com>
2018-05-07 20:58:52 +0900

    schedule agent as default

--- agents/config_agents.py
+++ agents/config_agents.py
@@ -6,7 +6,7 @@
 def config_agent(_flags):
     flags = _flags
 
-    flags.DEFINE_string("agent", "cfao_sa_generalized", "Agent")
+    flags.DEFINE_string("agent", "schedule", "Agent")
 
     flags.DEFINE_integer("training_step", 40000, "Training time step")
     flags.DEFINE_integer("testing_step", 1000, "Testing time step")
